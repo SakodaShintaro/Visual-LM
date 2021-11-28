78f3130d18a264caa0d7ac70355abf60b1a76039
sakoda <sakoda@keio.jp>
2021-11-28 09:28:04 +0900

    画像をモノクロに変更

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ def generage_image(text):
 
     # 画像サイズ，背景色，フォントの色を設定
     canvasSize = (300, 150)
-    backgroundRGB = (255, 255, 255)
-    textRGB = (0, 0, 0)
+    background_color = (255)
+    text_color = (0)
 
     # 文字を描く画像の作成
-    image = PIL.Image.new('RGB', canvasSize, backgroundRGB)
+    image = PIL.Image.new('L', canvasSize, background_color)
     draw = PIL.ImageDraw.Draw(image)
 
     # 用意した画像に文字列を描く
     textWidth, textHeight = draw.textsize(text)
     textTopLeft = (canvasSize[0] // 6, canvasSize[1] // 2 - textHeight // 2)  # 前から1/6，上下中央に配置
-    draw.text(textTopLeft, text, fill=textRGB)
+    draw.text(textTopLeft, text, fill=text_color)
 
     return image
 
@@ -27,3 +27,7 @@ texts = ["1+1=2", "2+2=4", "3+3=6"]
 for i, text in enumerate(texts):
     image = generage_image(text)
     image.save(f"{i}.png")
+
+for i in range(len(texts)):
+    image = PIL.Image.open(f"{i}.png")
+    print(image)
