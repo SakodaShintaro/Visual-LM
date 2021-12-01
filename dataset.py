e71508f6d9e8057f1a606862e92382e84324fe20
sakoda <sakoda@keio.jp>
2021-12-01 22:47:18 +0900

    縦横サイズを変更・文字を中央に配置するように変更

--- dataset.py
+++ dataset.py
@@ -7,7 +7,8 @@ from constant import *
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self):
-        self.operation_list = ["plus", "minus", "mult"]
+        # self.operation_list = ["plus", "minus", "mult"]
+        self.operation_list = ["plus", "minus"]
         self.term_num = 2
         self.upper_bound = 100
         self.num_per_op = (self.upper_bound ** self.term_num)
@@ -41,21 +42,21 @@ class Dataset(torch.utils.data.Dataset):
         # 参考) https://qiita.com/implicit_none/items/a9bf7eebe125c9d773eb
 
         # 画像サイズ，背景色，フォントの色を設定
-        canvasSize = IMAGE_SIZE
+        canvas_size = IMAGE_SIZE[::-1]
         background_color = (255)
         text_color = (0)
 
         # 文字を描く画像の作成
-        image = PIL.Image.new('L', canvasSize, background_color)
+        image = PIL.Image.new('L', canvas_size, background_color)
         draw = PIL.ImageDraw.Draw(image)
 
         font = PIL.ImageFont.truetype("/usr/share/fonts/truetype/fonts-japanese-gothic.ttf", 16)
 
         # 用意した画像に文字列を描く
-        textWidth, textHeight = draw.textsize(text, font=font)
-        textTopLeft = (canvasSize[0] // 8, canvasSize[1] // 2 - textHeight // 2)  # 前から1/6，上下中央に配置
-        assert textTopLeft[1] + textHeight < IMAGE_WIDTH
-        assert textTopLeft[0] + textWidth < IMAGE_WIDTH
-        draw.text(textTopLeft, text, font=font, fill=text_color)
+        text_width, text_height = draw.textsize(text, font=font)
+        text_pos = (canvas_size[0] // 2 - text_width // 2, canvas_size[1] // 2 - text_height // 2)  # 上下左右中央に配置
+        assert text_pos[1] + text_height < IMAGE_HEIGHT
+        assert text_pos[0] + text_width < IMAGE_WIDTH
+        draw.text(text_pos, text, font=font, fill=text_color)
 
         return image
